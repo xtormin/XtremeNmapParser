5a592a0a6444399bc5e19c2a7df31786f714dfc4
Xtormin <xtormin@protonmail.com>
2023-06-08 22:56:55 +0200

    Refactor functions

--- app/modules/output_format.py
+++ app/modules/output_format.py
@@ -9,6 +9,7 @@ logger = CustomLogger('test')
 config = confuse.Configuration('XNP', __name__)
 config.set_file('config/config.yaml')
 
+NMAP_FILE_EXTENSION = config['nmap_file_extension'].get()
 HEADERS = config['xlsx']['headers'].get()
 SHEET_NAME = config['xlsx']['sheet_name'].get()
 HEADERS_COLOR = config['xlsx']['headers_color'].get()
@@ -25,52 +26,41 @@ def adjust_columns(worksheet, df):
         # set the column length
         worksheet.set_column(i, i, column_len)
 
+def header_format_style(workbook):
+    header_format = workbook.add_format()
+    header_format.set_bg_color(HEADERS_COLOR)
+    header_format.set_bold()
+    header_format.set_font_color(HEADERS_TEXT_COLOR)
+    header_format.set_center_across()
+    return header_format
+
 def df_to_xlsx(df, filename):
     try:
         df = df[HEADERS]
-
         # Create a Pandas Excel Writer using Xlsxwriter as the engine
         writer = pd.ExcelWriter(filename, engine='xlsxwriter')
-
         # Converts the dataframe to a Xlsxwriter Excel Object
         df.to_excel(writer, sheet_name=SHEET_NAME, index=False)
-
         # Get the xlsxwriter workbook and worksheet objects
         workbook = writer.book
         worksheet = writer.sheets[SHEET_NAME]
         # Tab color
         worksheet.set_tab_color(HEADERS_COLOR)
-
         # Get the dimensions of the database
         (max_row, max_col) = df.shape
-
-        # Worksheet format
-        center_format = workbook.add_format().set_center_across()
-        bold = workbook.add_format().set_bold()
-
         # Columns format
         adjust_columns(worksheet, df)
-
-
         # Header format
-        header_format = workbook.add_format()
-        header_format.set_bg_color(HEADERS_COLOR)
-        header_format.set_bold()
-        header_format.set_font_color(HEADERS_TEXT_COLOR)
-        header_format.set_center_across()
-
+        header_format = header_format_style(workbook)
         # Table headers with custom format
         table_headers = []
-        for column in HEADERS: table_headers.append({'header': column, 'header_format': header_format})
-
+        table_headers = [{'header': column, 'header_format': header_format} for column in HEADERS]
         # Create table with custom format
         worksheet.add_table(0, 0, max_row, max_col - 1, {'name': 'NmapScanData',
                                                          'style': TABLE_STYLE,
                                                          'columns': table_headers })
-
         # Close XLSX file
         writer.close()
-
         logger.info(f" |+| Output | xlsx | {filename}")
     except Exception as e:
         logger.error(f" |x| Error | {filename} file not created")
@@ -78,11 +68,7 @@ def df_to_xlsx(df, filename):
 
 def df_to_csv(df, filename):
     try:
-        df.to_csv(filename,
-                  sep=';',
-                  encoding='utf-8',
-                  header=None,
-                  index=False)
+        df.to_csv(filename, sep=';', encoding='utf-8', header=None, index=False)
         logger.info(f" |+| Output | csv | {filename}")
     except Exception as e:
         logger.error(f" |x| Error | {filename} file not created")
@@ -95,3 +81,27 @@ def df_to_json(df, filename):
     except Exception as e:
         logger.error(f" |x| Error | {filename} file not created")
         logger.error(e)
+
+def get_output_name(file_xml, output_name, merger):
+    if file_xml:
+        output_name = file_xml.split(NMAP_FILE_EXTENSION)[0]
+
+    if merger:
+        if not output_name:
+            output_name = "merged_nmap_scan_data"
+
+    return output_name
+def write_dataframe(df, list_output_format, file_output_name=None, merger=None, file_xml=None):
+    if df.empty:
+        logger.warning(f" |?| Warning | The file has no data, omitting export")
+    else:
+        output_name = get_output_name(file_xml, file_output_name, merger)
+
+        for output_format_type in list_output_format:
+            output_file_xml = f"{output_name}.{output_format_type}"
+            if output_format_type == "csv":
+                df_to_csv(df, output_file_xml)
+            elif output_format_type == "xlsx":
+                df_to_xlsx(df, output_file_xml)
+            elif output_format_type == "json":
+                df_to_json(df, output_file_xml)
