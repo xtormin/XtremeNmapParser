d42fc36e69ff63f64bfdb722adc031fd9f689bf0
Xtormin <xtormin@protonmail.com>
2023-06-13 21:13:37 +0200

    Moved function from app.py

--- app/utils/functions.py
+++ app/utils/functions.py
@@ -1,4 +1,20 @@
 import os
+import confuse
+
+# LOAD CONFIG FROM YAML FILE
+config = confuse.Configuration('XNP', __name__)
+config.set_file('config/config.yaml')
+
+APPNAME = config['appname'].get()
+NMAP_FILE_EXTENSION = config['nmap_file_extension'].get()
 
 def get_dir_files(dir):
     return os.listdir(dir)
+
+def get_dir_files_recursive(folder):
+    xml_files = []
+    for root, dirs, files in os.walk(folder):
+        for file in files:
+            if file.endswith(NMAP_FILE_EXTENSION):
+                xml_files.append(os.path.join(root, file))
+    return xml_files
