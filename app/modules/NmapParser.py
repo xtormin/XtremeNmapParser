7a2b4b2a483a9e2ddaeb597b8e072bac1afd1915
Xtormin <xtormin@protonmail.com>
2023-06-13 21:16:15 +0200

    Update the data type of ScanData

--- app/modules/NmapParser.py
+++ app/modules/NmapParser.py
@@ -13,7 +13,7 @@ logger = CustomLogger('test')
 config = confuse.Configuration('XNP', __name__)
 config.set_file('config/config.yaml')
 
-HEADERS = config['xlsx']['headers'].get()
+HEADERS_ALL = config['xlsx']['headers_all'].get()
 
 class NmapParser:
     def __init__(self, xml_file=None):
@@ -24,30 +24,32 @@ class NmapParser:
             nmap_report = NmapXMLReport(self.xml_file)
             all_rows_dataframe = []
             for host in nmap_report.hosts:
-                dataframe = ScanData()
-                for hostname in host.hostnames:
-                    for host_name in hostname.hostnames:
-                        dataframe.hostname = host_name.name
-                for address in host.addresses:
-                    dataframe.addr = address.addr
-                for status in host.status:
-                    dataframe.host_state = status.state
                 for port in host.ports:
-                    dataframe.portid = port.portid
-                    dataframe.protocol = port.protocol
+                    dataframe = ScanData()
+                    for hostname in host.hostnames:
+                        for host_name in hostname.hostnames:
+                            dataframe.data["Hostname"] = host_name.name
+                    for address in host.addresses:
+                        dataframe.data["IP"] = address.addr
+                    for status in host.status:
+                        dataframe.data["State"] = status.state
+                    dataframe.data["Port"] = port.portid
+                    dataframe.data["Protocol"] = port.protocol
+                    script_data = []
+                    for script in port.script:
+                        script_data.append(script.__str__())
+                    dataframe.data["Scripts"] = script_data
                     for state in port.state:
-                        dataframe.port_state = state.state
+                        dataframe.data["State Port"] = state.state
                     for service in port.service:
-                        dataframe.service_name = service.name
-                        dataframe.service_product = service.product
-                        dataframe.service_version = service.version
-                        dataframe.service_extrainfo = service.extrainfo
+                        dataframe.data["Service Name"] = service.name
+                        dataframe.data["Product"] = service.product
+                        dataframe.data["Version"] = service.version
+                        dataframe.data["Extrainfo"] = service.extrainfo
+                    all_rows_dataframe.append(dataframe)
 
-                        all_rows_dataframe.append(dataframe.to_list())
-
-            df = dataframe.to_dataframe(HEADERS, all_rows_dataframe)
+            df = ScanData.to_dataframe(all_rows_dataframe)
             logger.info(f" |+| {self.xml_file} parsed successfully  ")
-
             return df
 
         except etree.ParseError as EPE:
@@ -104,12 +106,6 @@ class NmapParser:
         # Sort by 'IP', 'Port', 'State', 'RelevantDuplicate' (in descending order so larger counts come first), then drop duplicates
         df = df.sort_values(by=['IP', 'Port', 'State', 'RelevantDuplicate'], ascending=[True, True, False, False])
         df = df.drop_duplicates(subset=['IP', 'Port'], keep='first')
-
-        # Convert 'Port' to int for proper sorting
-        df['Port'] = df['Port'].astype(int)
-
-        # Sort final dataframe by 'IP' and then 'Port'
-        df = df.sort_values(by=['IP', 'Port'])
 
         df['Hostname'] = df.groupby('IP')['Hostname'].transform(
             lambda x: x.fillna(method='ffill').fillna(method='bfill'))
