26ba819978d15bece2914f08a13870deb8319611
Xtormin <xtormin@protonmail.com>
2023-06-13 21:13:08 +0200

    New arguments in banner.py

--- app/utils/banner.py
+++ app/utils/banner.py
@@ -24,18 +24,20 @@ def main():
 
     print(colored(BANNER, 'red', attrs=['bold']))
 
-def print_arguments_info(single_xml, folder_multiple_xml, list_output_format, file_output_name, merger, recursive):
-
+def print_arguments_info(single_xml, folder_multiple_xml, list_output_format, file_output_name, merger, recursive, df_columns, only_open_ports):
+    df_columns = " ".join(df_columns)
     BANNER = f"""  
  --------------------------------------------------------------  
  | Arguments information  
  --------------------------------------------------------------  
- | File (-f)          | {single_xml}  
- | Folder (-d)        | {folder_multiple_xml}  
- | Merge files (-M)   | {merger}  
- | Recursive (-R)     | {recursive}  
- | Output format (-o) | {list_output_format}  
- | Output name (-O)   | {file_output_name}  
+ | File (-f)           | {single_xml}  
+ | Folder (-d)         | {folder_multiple_xml}  
+ | Merge files (-M)    | {merger}  
+ | Recursive (-R)      | {recursive}  
+ | Output format (-o)  | {list_output_format}  
+ | Output name (-O)    | {file_output_name}
+ | Columns (-C)        | {df_columns}
+ | Open ports (--open) | {only_open_ports}
  --------------------------------------------------------------  
 """
     print(colored(BANNER, 'blue', attrs=['bold']))
